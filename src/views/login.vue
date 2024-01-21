0ea5e6749b610d87835bd6dedd7b8c14bf1ea413
nearzero32 <61017871+nearzero32@users.noreply.github.com>
2024-01-21 21:13:13 +0300

    1

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,28 +1,39 @@
 <template>
 <div class="continer">
   <div class="content">
-    <img src="@/assets/alwizara-removebg-preview.png" alt="">
+    <img class="logo" src="@/assets/alwizara-removebg-preview.png" alt="">
+    <div class="text">
+      <span>جمهورية العراق</span>
+      <h4>وزارة التربية محافظة المثنى</h4>
+      <h4>المديرية العامة للتربية في المثنى</h4>
+    </div>
   </div>
   <div class="login">
-    <h1>Login</h1>
-      <input type="text" class="orderNumber" placeholder="شماره سفارش">
-      <input type="text" class="idNumber" placeholder="شماره سفارش">
-      <v-btn block>Login</v-btn>
+    <h1>تسجيل دخول</h1>
+      <input type="text" class="orderNumber" placeholder="رقم الطلب">
+      <input type="text" class="idNumber" placeholder="رقم الهوية">
+      <v-btn block @click="$router.push('work_register')">تسجيل</v-btn>
   </div>
 </div>
 </template>
-// جمهورية العراق وزارة التربية محافظة المثنى المديرية العامة لتربية للتربية في المثنى
+// جمهورية العراق وزارة التربية محافظة المثنى المديرية العامة للتربية في المثنى
 
 <style lang="scss" scoped>
 
-@mixin query($size){
+@mixin maxQuery($size){
+  @media (max-width: #{$size}){
+    @content;
+  }
+}
+@mixin minQuery($size){
   @media (max-width: #{$size}){
     @content;
   }
 }
   $minWidth:100%;
-  $mainColor: salmon;
-  $accColor: yellow;
+  $mainColor: #616F39;
+  $accColor: #FFB000;
+  $lastColor: #FFF4F4;
   // vars
   %centering{
     display: flex;
@@ -36,21 +47,91 @@
     flex-direction: row-reverse;
     height: 100vh;
     width: 100%;
-  }
-  .login{
-    @extend %centering;
     gap: 2rem;
-    width: 80%;
-    h1{
-      font-size: 2.4rem;
+    .content{
+      @extend %centering;
+      justify-content: space-evenly;
+      width: 30%;
+      height: 100%;
+      background-color: #{$mainColor};
+      .logo{
+        width: 40%;
+        margin-bottom: .5rem;
+      }
+      .text{
+        span{
+          font-size: 1.4rem;
+          margin-bottom: 0.5rem;
+          display: block;
+        }
+        padding: 1.5rem;
+        text-align: center;
+        width: 100%;
+        color: #{$lastColor};
+      }
     }
-    .orderNumber,.idNumber{
+    .login{
+      @extend %centering;
+      padding-left: 2rem;
+      gap: 2rem;
+      width: 80%;
+      h1{
+        font-size: 2.4rem;
+      }
+      .orderNumber,.idNumber{
+        width: 100%;
+        padding: 5px 15px ;
+        outline: none;
+        direction: rtl;
+        border-bottom: 1px solid #{$mainColor};
+      }
+
+    }
+    .v-btn{
+      background-color: #{$mainColor};
+      color: #{$lastColor};
       width: 100%;
-      background-color: #{$mainColor} ;
-      padding: 5px 15px ;
-      outline: none;
-      direction: rtl;
-      border-bottom: 1px solid #{$accColor};
+      border-radius: 8px;
+      &:hover{
+        background-color: #{$accColor};
+      }
+    }
+  }
+
+  @include maxQuery(1023px){
+    .login{
+      padding-left: 0;
+    }
+    .continer{
+      flex-direction: column;
+      justify-content: start;
+      .content{
+        flex-direction: row;
+        width: 100%;
+        height: 30%;
+        border-bottom-left-radius:18px ;
+        border-bottom-right-radius:18px ;
+        .logo{
+        width: 30%;
+        max-width: 150px;
+      }
+      .text{
+          padding: 0;
+          width: 50%;
+          span{
+            font-size: 1rem;
+          }
+          h4{
+            margin-top: .5rem;
+            font-size: .9rem;
+          }
+        }
+      }
+    }
+  }
+  @include maxQuery(700px){
+    h1{
+      font-size: 1.8rem !important;
     }
   }
 </style>
